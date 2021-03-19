7a517d9188551f6d13b995fb6edb984d4da8d30d
czelijm <czelijm@outlook.com>
2021-03-19 16:21:17 +0100

    reading json file from github

---
+++ js/render-projects-from-json.js
@@ -0,0 +1,13 @@
+function renderProjectsFromJson(){
+   var filtersDiv = document.getElementById("filters");
+//    var jsonCategories = 
+   var categories = [];
+   var categoriesWithHtml = [];
+   
+   
+}
+
+renderProjectsFromJson();
+$.getJSON("js/assets/projects.json",(data)=>{
+    console.log(data);
+})
